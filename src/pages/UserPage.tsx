4b22748fadd3df46d258b77673a25ca7e9ee5e65
Shkettoe <103437209+Shkettoe@users.noreply.github.com>
2023-01-17 21:15:37 +0100

    fixed a bug with profile redirection

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,4 +1,3 @@
-import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
@@ -29,7 +28,7 @@ const UserPage = () => {
                 console.log(err)
             }
         })()
-    }, [])
+    }, [params])
     useEffect(()=>{
         (async ()=>{
             
