321689c13c7d6d2c0b8a2befe2381e1f216c5e1b
Shkettoe <103437209+Shkettoe@users.noreply.github.com>
2023-01-17 20:23:40 +0100

    profile pages make sense now

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -7,13 +7,13 @@ import { Content, Grid2, OrangeText } from '../assets/Common.style'
 import { ImgST } from '../assets/Img.style'
 import { ImgVars } from '../assets/Vars'
 import QuoteCard from '../components/QuoteCard'
-import { GetHelp } from '../helpers/Get.helper'
 import { Quote } from '../interfaces/models/Quote.interface'
 import { User } from '../interfaces/models/User.interface'
 import { Vote } from '../interfaces/models/Vote.interface'
 import { Get } from '../services/Get.request'
 
 const UserPage = () => {
+    const { votes }: User = useSelector((state: any) => state?.user.value)
     const params = useParams()
     const [karma, setKarma] = useState(0)
     const [user, setUser] = useState<User | null>(null)
@@ -59,25 +59,41 @@ const UserPage = () => {
                 <div>
                     <ListHeading><OrangeText>Most liked quotes</OrangeText></ListHeading>
                     {mostLiked.map((q: Partial<Quote>) => {
+                        let vote: string = ""
+                        for (const iterator of votes) {
+                            if (iterator.post?.id === q.id) {
+                                vote = iterator.val ? "upvote" : "downvote"
+                            }
+                        }
                         return (
-                            <QuoteCard id={q.id} user_id={!params.id ? user?.id : undefined} key={q.id} img={user?.avatar} author={user?.first_name + " " + user?.last_name} content={q.content || ""} score={q.score || 0} />
+                            <QuoteCard vote={vote} id={q.id} user_id={!params.id ? user?.id : undefined} key={q.id} img={user?.avatar} author={user?.first_name + " " + user?.last_name} content={q.content || ""} score={q.score || 0} />
                         )
                     })}
                 </div>
                 <div>
                     <ListHeading>Most recent</ListHeading>
                     {recent.map((q: Partial<Quote>) => {
+                        let vote: string = ""
+                        for (const iterator of votes) {
+                            if (iterator.post?.id === q.id) {
+                                vote = iterator.val ? "upvote" : "downvote"
+                            }
+                        }
                         return (
-                            <QuoteCard id={q.id} user_id={!params.id ? user?.id : undefined} key={q.id} img={user?.avatar} author={user?.first_name + " " + user?.last_name} content={q.content || ""} score={q.score || 0} />
+                            <QuoteCard vote={vote} id={q.id} user_id={!params.id ? user?.id : undefined} key={q.id} img={user?.avatar} author={user?.first_name + " " + user?.last_name} content={q.content || ""} score={q.score || 0} />
                         )
                     })}
                 </div>
                 <div>
                     <ListHeading>Liked</ListHeading>
                     {user?.votes.map((v: Vote)=>{
+                        let vote: string = ""
+                        for (const iterator of votes) {
+                            vote = iterator.val ? "upvote" : "downvote"
+                        }
                         if(v.val)
                         return(
-                            <QuoteCard user_id={v.post?.user?.id} vote='upvote' id={v.post?.id} key={v.post?.id} img={v.post?.user?.avatar} author={`${v.post?.user?.first_name} ${v.post?.user?.last_name}`} content={v.post?.content || ""} score={v.post?.score || 0} />
+                            <QuoteCard vote={vote} user_id={v.post?.user?.id} id={v.post?.id} key={v.post?.id} img={v.post?.user?.avatar} author={`${v.post?.user?.first_name} ${v.post?.user?.last_name}`} content={v.post?.content || ""} score={v.post?.score || 0} />
                         )
                     })}
                 </div>
